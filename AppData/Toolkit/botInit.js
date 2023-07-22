2ebeb3dd4e094fade4f6b0f109a808778b50cf41
RatWasHere <lleulescualin@gmail.com>
2023-07-23 02:49:58 +0300

    kill me

--- AppData/Toolkit/botInit.js
+++ AppData/Toolkit/botInit.js
@@ -1,7 +1,7 @@
 const runCmd = require('node-run-cmd');
 const customFs = require('fs')
 
-const filePath = './AppData/bot.js';
+const filePath = './bot.js';
 function consoleLog(log) {
   document.getElementById('console').innerHTML += `<div class="fade" style="margin: auto; margin-top: 6px;">${ansiToHtml(log)}</div>`
 }
